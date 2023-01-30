ca82389f51a8e1ed3c434ea0162b7156a811ce8f
liljana ristevska <li.ristevska@gmail.com>
2023-01-30 13:26:14 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/g4_jpa_assignment/G4JpaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g4_jpa_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G4JpaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G4JpaAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g4_jpa_assignment/G4JpaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g4_jpa_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G4JpaAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
