9d36b3e1a0fd9e8170b55351f2b734ca9a0a59bc
SamuelSvahn <samuelsvahn@hotmail.com>
2023-01-31 09:18:25 +0100

    My part is done

--- src/main/java/se/lexicon/g4_jpa_assignment/entity/Ingredient.java
+++ src/main/java/se/lexicon/g4_jpa_assignment/entity/Ingredient.java
@@ -1,18 +1,13 @@
 package se.lexicon.g4_jpa_assignment.entity;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-@Setter
-@Getter
-@EqualsAndHashCode
-@ToString
+
+@Data
 @Entity
 public class Ingredient {
 
